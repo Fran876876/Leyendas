fad901df6f3fc29e9a845075e47ff7879227091b
Fran876876 <fmartosmuoz@yahoo.es>
2025-03-13 19:06:25 +0100

    hola

---
+++ app/src/main/java/com/summercompany/leyendas/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.summercompany.leyendas
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    var casa = 12
+
+    var contador = 1
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
